f245c367a4f1a615a5db3fefc3b9a1505cc7f9c0
Centurion83 <Ericharper27@gmail.com>
2019-09-14 08:57:09 -0700

    changed path from personal. Make sure to add your path
    Edit

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 #/bin/bash
  repository="git@github.com:Centurion83/local_manifest.xml.git"
- localFolder="/home/centurion/scripts_local"
+ localFolder="/home/user/scripts_local"
 git clone "$repository" "$localFolder"
