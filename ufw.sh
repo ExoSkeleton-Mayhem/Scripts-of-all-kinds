49da88a68c2cfeda3c8c8e92c841543900c2a0dc
Eric <ericharper27@gmail.com>
2019-10-11 12:08:36 -0700

    fixed ufw

--- ufw.sh
+++ ufw.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 # Script to open all important ports on ufw
-echo sudo ufw allow 8080/tcp
+echo sudo 'ufw allow 8080/tcp'
 sleep 1
-echo sudo ufw allow 443/tcp
+echo sudo 'ufw allow 443/tcp'
 sleep 2
-echo sudo ufw allow 80/tcp
+echo sudo 'ufw allow 80/tcp'
 sleep 3
-echo sudo ufw allow 4422/tcp
+echo sudo 'ufw allow 4422/tcp'
 sleep 4
-echo sudo ufw allow ssh
+echo sudo 'ufw allow ssh'
 sleep 5
-echo sudo ufw status -verbose
+echo sudo 'ufw status -verbose'
 sleep 6
-echo sudo ufw enable
+echo sudo 'ufw enable'
